3cc9779ac52020dd168398bf038f72e7a76444ef
Luciano Ratamero <luciano@ratamero.com>
2021-10-12 14:37:17 -0300

    Use body as svelte root

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		target: 'body',
 		adapter: adapter(),
 	}
 };
