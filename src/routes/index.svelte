e1247cedd61f47e11d3f5451db4d2130ea13c4fe
Luciano Ratamero <luciano@ratamero.com>
2021-10-12 14:37:33 -0300

    Improves layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,33 @@
-<div class='container'>
+<article class='min-vh-100'>
 
-  <img src='/pythonmao_2.svg' alt='PythonBrasil 2022 - Manaus'>
-  <h1>15 a 23 de Outubro</h1>
-  <h2><a href='https://goo.gl/maps/zxSjP739d96Y6gYYA'>Centro de Convenções do Amazonas Vasco
-    Vasques</a></h2>
-
-  <section id='o-evento'>
-    <h1>O Evento</h1>
-
-    <p>A PythonBrasil é o maior evento sobre linguagem de programação Python do Brasil. Feito pela comunidade para a
-      comunidade, tem o objetivo de difundir a linguagem, promover a troca de experiências e manter a comunidade
-      crescendo igualmente em público e impacto social.</p>
-
-    <p>Serão 6 dias de imersão onde os participantes poderão contribuir para projetos de software livre, participar de
-      treinamentos e adquirir novos conhecimentos com desenvolvedores renomados da comunidade. A programação está
-      organizada da seguinte forma: Tutoriais (23 e 24 de outubro no Centro Universitário Estácio de Sá), Palestras e
-      Keynotes (25 a 27 de outubro no Centro de Convenções de Ribeirão Preto, CCRP) e Sprints (28 de outubro no Centro
-      Universitário Estácio de Sá).</p>
-
-    <p>Este ano, está sendo organizada pela comunidade Python de Ribeirão Preto/SP com o apoio da APyB (Associação
-      Python Brasil).</p>
+  <section id='sobre' class='d-flex flex-column justify-content-center align-items-center min-vh-100'>
+    <div class='container'>
+      <img src='/pythonmao_2.svg' alt='PythonBrasil 2022 - Manaus'>
+      <h1>15 a 23 de Outubro</h1>
+      <h2><a href='https://goo.gl/maps/zxSjP739d96Y6gYYA'>Centro de Convenções do Amazonas Vasco
+        Vasques</a></h2>
+    </div>
   </section>
-</div>
+
+  <section id='o-evento' class='d-flex flex-column justify-content-center align-items-center min-vh-100'>
+    <div class='container'>
+      <h1>O Evento</h1>
+
+      <p>A PythonBrasil é o maior evento sobre linguagem de programação Python do Brasil. Feito pela comunidade para a
+        comunidade, tem o objetivo de difundir a linguagem, promover a troca de experiências e manter a comunidade
+        crescendo igualmente em público e impacto social.</p>
+
+      <p>Serão 6 dias de imersão onde os participantes poderão contribuir para projetos de software livre, participar de
+        treinamentos e adquirir novos conhecimentos com desenvolvedores renomados da comunidade. A programação está
+        organizada da seguinte forma: Tutoriais (23 e 24 de outubro no Centro Universitário Estácio de Sá), Palestras e
+        Keynotes (25 a 27 de outubro no Centro de Convenções de Ribeirão Preto, CCRP) e Sprints (28 de outubro no Centro
+        Universitário Estácio de Sá).</p>
+
+      <p>Este ano, está sendo organizada pela comunidade Python de Ribeirão Preto/SP com o apoio da APyB (Associação
+        Python Brasil).</p>
+    </div>
+  </section>
+</article>
 
 <style>
   :global(body) {
@@ -29,16 +35,11 @@
     padding: 0;
   }
 
-  img {
-    max-height: 20vh;
+  #o-evento {
+    background: #FFD167;
   }
 
-  div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    min-height: 100vh;
-    align-items: center;
-    text-align: center;
+  img {
+    max-height: 20vh;
   }
 </style>
