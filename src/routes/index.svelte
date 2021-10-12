288500d670c9598eda9bc9aad328e436e89f6df3
Luciano Ratamero <luciano@ratamero.com>
2021-10-12 13:42:16 -0300

    add sample pages

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href='https://kit.svelte.dev'>kit.svelte.dev</a> to read the documentation</p>
+
+<p>{counter}</p>
+<button on:click={(event) => {
+	console.log(event);
+	counter += 1;
+}}>incrementar
+</button>
+
+<a href='/about'>bdsyu</a>
+
+<script>
+	let counter = 0;
+</script>
+
+<style>
+	p { color: red; }
+</style>
